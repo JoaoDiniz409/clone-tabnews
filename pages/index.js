392c65dc77c09e9e764cc5ab33fadf219f1d1713
JoaoDiniz409 <77306405+JoaoDiniz409@users.noreply.github.com>
2023-08-24 01:08:05 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mesmo que o céu caia, há sempre um caminho.</h1>
+    return <h1>Mesmo que o céu caia, há sempre um caminho. - It’s Okay That’s Love</h1>
 }
 
 export default Home;
