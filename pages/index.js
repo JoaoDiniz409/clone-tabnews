b6283c660520442834e73bce957efceba37ae7dd
JoaoDiniz409 <77306405+JoaoDiniz409@users.noreply.github.com>
2023-08-27 00:26:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>"PARABÉNS, DUDAAA! Quero te desejar um parabéns especial desta vez, porque você merece</h1>
+  return (
+    <h1>
+      "PARABÉNS, DUDAAA! Quero te desejar um parabéns especial desta vez, porque
+      você merece
+    </h1>
+  );
 }
 
 export default Home;
